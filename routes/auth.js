44061e01cb24816a6c54f969f6f54d778e3f460e
Yimeng Yu <yimeng.yu@gmail.com>
2020-05-31 21:46:17 -0700

    add auth route

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,60 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const auth = require("../middleware/auth");
+const { check, validationResult } = require("express-validator");
+
+const User = require("../models/User");
+
+
+//@route post api/auth
+//@desc Auth user & get token, validate user's email and password and then get the token
+//@access private
+
+router.post("/", [
+    check("email", "Please include a valid email").isEmail(),
+    check("password", "password is required").exists(),
+],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        const { email, password } = req.body;
+        try {
+            let user = await User.findOne({ email: email });
+            if (!user) {
+                return res.status(400).json({ msg: "Invalid Credentials" })
+            }
+
+            const isMatch = await bcrypt.compare(password, user.password);
+            if (!isMatch) {
+                return res.status(400).son({ msg: "Invalid Credntials" })
+            }
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            };
+            jwt.sign(
+                payload,
+                config.get("jwtSecret"),
+                {
+                    expiresIn: 36000
+                },
+                (err, token) => {
+                    if (err) throw err;
+                    res.json({ token });
+                }
+            )
+        }
+        catch (err) {
+            console.error(err.message);
+            res.status(500).send("Server Error");
+        }
+    })
 
 
 //@route POST api/auth
@@ -7,8 +62,14 @@ const router = express.Router();
 //@access private
 
 
-router.get("/", (req, res) => {
-    res.send("user login")
+router.get("/", auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select("-password");
+        res.json(user);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
 });
 
 module.exports = router;
