579e718f0f6d5b0438b82d6cdaa40acaf29920f8
Yimeng Yu <yimeng.yu@gmail.com>
2020-06-03 22:11:21 -0700

    prepare to deploy

--- server.js
+++ server.js
@@ -3,14 +3,22 @@ const connectDB = require("./config/db")
 const app = express();
 
 connectDB();
-app.use(express.json({ extended: false }))
+app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) =>
-    res.json({ msg: "Hello world" }))
 
 app.use("/api/users", require("./routes/users"));
 app.use("/api/tasks", require("./routes/tasks"));
 app.use("/api/auth", require("./routes/auth"));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    );
+}
 
 const PORT = process.env.PORT || 5000;
 
